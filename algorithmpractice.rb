2eaf327df2fc13351d31280ce24e0141b6e41650
Nate <natelalvarado@gmail.com>
2021-11-18 21:35:16 -0500

    Complete reduce algorithm problem

---
+++ algorithmpractice.rb
@@ -0,0 +1,15 @@
+# Write a function that returns the sum of all numbers in a given array.
+
+# Input: [1, 2, 3, 4]Output: 10
+
+# def calculate_sum(array)
+#   sum = 0 
+#   i = 0 
+#   while i < array.length
+#     sum += array[i]
+#     i += 1
+#   end
+#   return sum
+# end
+
+# p calculate_sum([1, 2, 3, 4])
