8cd240bcee16f151259cb09df0570e12347f2133
Nate <natelalvarado@gmail.com>
2021-12-28 13:33:36 -0500

    Adds Reverse String algorithm problem

--- algorithmpractice.rb
+++ algorithmpractice.rb
@@ -121,6 +121,10 @@ Input:[2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
 Output:[2, 3, 1, 2, 2, 1, 5, 2, 2]
 
 #----------------------------------------------------------------------------------------------------------------------------------------
+Write a function that returns the reverse of a given string.
+
+Input: “abcde”Output: “edcba”
+#----------------------------------------------------------------------------------------------------------------------------------------
 # Given a year, report if it is a leap year. The tricky thing here is that a leap year in the Gregorian calendar occurs: 
 # on every year that is evenly divisible by 4
 # except every year that is evenly divisible by 100
--- deliberatepractice.rb
+++ deliberatepractice.rb
@@ -93,63 +93,63 @@
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
 
 #----------------------------------------------------------------------------------------------------------------------------------------
-Nested Loops
+# Nested Loops
 
-#1
-Write a function that accepts an array of numbers, and returns an array of the products of every pair of numbers from the array.
+# #1
+# Write a function that accepts an array of numbers, and returns an array of the products of every pair of numbers from the array.
  
-Example:
-Input: [3, 4, 5, 6]
-Output: [12, 15, 18, 20, 24, 30]
-Explanation: (3 * 4, 3 * 5, 3 * 6, 4 * 5, 4 * 6, 5 * 6)
+# Example:
+# Input: [3, 4, 5, 6]
+# Output: [12, 15, 18, 20, 24, 30]
+# Explanation: (3 * 4, 3 * 5, 3 * 6, 4 * 5, 4 * 6, 5 * 6)
 
-def productpair(array)
+# def productpair(array)
   
-end
+# end
  
-#2
-Write a function that accepts two arrays of numbers, and returns an array of the products of every number from the first array multiplied by every number of the second array:
-Example:
-Input: Two arrays: [1, 2], [3, 4, 5]
-Output: [3, 4, 5, 6, 8, 10]
-Explanation: (1 * 3, 1 * 4, 1 * 5, 2 * 3, 2 * 4, 2 * 5)
+# #2
+# Write a function that accepts two arrays of numbers, and returns an array of the products of every number from the first array multiplied by every number of the second array:
+# Example:
+# Input: Two arrays: [1, 2], [3, 4, 5]
+# Output: [3, 4, 5, 6, 8, 10]
+# Explanation: (1 * 3, 1 * 4, 1 * 5, 2 * 3, 2 * 4, 2 * 5)
  
-#3
-Write a function that accepts an array of numbers and returns true or false, depending on whether there is a pair of two numbers that add up to 10.
+# #3
+# Write a function that accepts an array of numbers and returns true or false, depending on whether there is a pair of two numbers that add up to 10.
  
-Examples:
-Input: [2, 4, 6, 7]
-Output: true (because 4 + 6 == 10)
+# Examples:
+# Input: [2, 4, 6, 7]
+# Output: true (because 4 + 6 == 10)
  
-Second Example:
-Input: [1, 2, 3, 4]
-Output: false (because there is no combination of two numbers that add up to 10)
+# Second Example:
+# Input: [1, 2, 3, 4]
+# Output: false (because there is no combination of two numbers that add up to 10)
  
-#4
-Write a function that accepts an array of numbers, and returns the largest product of any pair of numbers within a given array.
+# #4
+# Write a function that accepts an array of numbers, and returns the largest product of any pair of numbers within a given array.
  
-Example:
-Input: [5, -2, 1, -9, -7, 2, 6] 
-Output: 63
-Explanation: (This is -9 * -7)
+# Example:
+# Input: [5, -2, 1, -9, -7, 2, 6] 
+# Output: 63
+# Explanation: (This is -9 * -7)
  
-#5
-Write a function that accepts two arrays of numbers, and returns the largest sum of a number from the first array added to a number from the second array.
+# #5
+# Write a function that accepts two arrays of numbers, and returns the largest sum of a number from the first array added to a number from the second array.
  
-Example: 
-Input: Two arrays: [5, 2, 1], [7, 8, 4, 1]
-Output: 13
-Explanation: (5 from the first array + 8 from the second array)
+# Example: 
+# Input: Two arrays: [5, 2, 1], [7, 8, 4, 1]
+# Output: 13
+# Explanation: (5 from the first array + 8 from the second array)
  
-#6
-Write a function that accepts two arrays of numbers, and returns an array that is the “intersection” of the two arrays, meaning an array of the numbers that are shared by the two original arrays.
+# #6
+# Write a function that accepts two arrays of numbers, and returns an array that is the “intersection” of the two arrays, meaning an array of the numbers that are shared by the two original arrays.
  
-Example: Two arrays: [5, 4, 3, 2, 1], [0, 2, 4, 6]
-Output: [2, 4] (or [4, 2])
-Explanation: 2 and 4 are the only values shared by both input arrays.
+# Example: Two arrays: [5, 4, 3, 2, 1], [0, 2, 4, 6]
+# Output: [2, 4] (or [4, 2])
+# Explanation: 2 and 4 are the only values shared by both input arrays.
  
-#7
-Write a function that accepts an array of numbers, and returns the first duplicate value.
+# #7
+# Write a function that accepts an array of numbers, and returns the first duplicate value.
  
-Example: [5, 7, 8, 9, 8, 2, 1]
-Output: 8 (since there are two 8’s in the array)
+# Example: [5, 7, 8, 9, 8, 2, 1]
+# Output: 8 (since there are two 8’s in the array)
