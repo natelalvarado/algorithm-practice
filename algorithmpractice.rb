58e1374ef4839cbeea67c71f7fd5903d9c8ff358
Nate <natelalvarado@gmail.com>
2022-01-04 21:02:25 -0500

    Updates algorithm practice

--- algorithmpractice.rb
+++ algorithmpractice.rb
@@ -121,18 +121,18 @@
 # Output:[2, 3, 1, 2, 2, 1, 5, 2, 2]
 
 #----------------------------------------------------------------------------------------------------------------------------------------
-Write a function that returns the reverse of a given string.
+# Write a function that returns the reverse of a given string.
 
-Input: “abcde”Output: “edcba” 
+# Input: “abcde”Output: “edcba” 
 
 
 
- def reversed(string)
-  {
-    new_string  = ""
+#  def reversed(string)
+#   {
+#     new_string  = ""
     
-  }
- end
+#   }
+#  end
 
 #----------------------------------------------------------------------------------------------------------------------------------------
 # Given a year, report if it is a leap year. The tricky thing here is that a leap year in the Gregorian calendar occurs: 
