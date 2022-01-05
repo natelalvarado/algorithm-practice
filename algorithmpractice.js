d89b2605d1b165f260a8a7fed4044360c4746df3
Nate <natelalvarado@gmail.com>
2022-01-05 16:19:22 -0500

    Adds sum algorithm problem javascript

--- algorithmpractice.js
+++ algorithmpractice.js
@@ -1,17 +1,32 @@
 //#--------------------------------------------------------------------------------------------------------------------------------------
+// Write a function that returns the sum of all numbers in a given array.
+
+// Input: [1, 2, 3, 4] Output: 10
+
+function sum(arr) {
+  var total = 0;
+
+  for (var i = 0; i < arr.length; ++i) {
+    total += arr[i];
+  }
+  return total;
+}
+
+console.log(sum([1, 2, 3, 4]));
+//#--------------------------------------------------------------------------------------------------------------------------------------
 // Write a function that returns the reverse of a given string.
 
 // Input: “abcde”Output: “edcba”
 
-function reversed(string) {
-  let newString = "";
+// function reversed(string) {
+//   let newString = "";
 
-  for (let i = 0; i < string.length; i++) {
-    newString = string[i] + newString;
-  }
+//   for (let i = 0; i < string.length; i++) {
+//     newString = string[i] + newString;
+//   }
 
-  return newString;
-}
-console.log(reversed("racecar"));
+//   return newString;
+// }
+// console.log(reversed("Hello"));
 
 //#--------------------------------------------------------------------------------------------------------------------------------------
--- algorithmpractice.rb
+++ algorithmpractice.rb
@@ -1,6 +1,6 @@
 # Write a function that returns the sum of all numbers in a given array.
 
-# Input: [1, 2, 3, 4]Output: 10
+# Input: [1, 2, 3, 4] Output: 10
 
 # def calculate_sum(array)
 #   sum = 0 
