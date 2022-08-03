2aa3e3644efa4b542b96375241f429b7fbb391e4
Nate <natelalvarado@gmail.com>
2022-08-02 21:31:48 -0400

    Update algorithm problem: frequent letter

--- algorithmpractice.rb
+++ algorithmpractice.rb
@@ -243,3 +243,33 @@
   # def Youtube_videos(arr1, arr2)
   #   arr1 
   # end
+
+#----------------------------------------------------------------------------------------------------------------------------------------
+# Given a string, find the most commonly occurring letter.
+
+# Input: “peter piper picked a peck of pickled peppers”Output: “p”
+
+def frequent_letter(string)
+  letter_count= {}
+  index = 0 
+  most_frequent_letter = ""
+  most_frequent_count = 0
+  
+  while index < string.length
+    if letter_count[string[index]]
+      letter_count[string[index]] += 1 
+    else
+      letter_count[string[index]] = 1
+    end 
+
+    if letter_count[string[index]] > most_frequent_count
+      most_frequent_count = letter_count[string[index]]
+      most_frequent_letter = string[index]
+    end
+    index += 1
+  end
+  
+  return most_frequent_letter
+end 
+
+p frequent_letter("peter piper picked a peck of pickled peppers")
